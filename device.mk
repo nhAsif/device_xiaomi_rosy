62ec21c6be69f6142ee5806d7a2fb36e935b3ea0
ajitlenka30 <ajitlenka30@gmail.com>
2021-12-21 09:17:54 +0000

    rosy: deboat some gapps

--- device.mk
+++ device.mk
@@ -455,3 +455,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
     libstagefright_enc_common
+     
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
