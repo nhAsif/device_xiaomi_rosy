288f90c5dee134af172c87a3ec024065c1a26fd0
nhAsif <najmulhasan3609@gmail.com>
2022-01-24 09:30:52 +0000

    rosy: Arrowos bringup

--- prop.mk
+++ prop.mk
@@ -182,6 +182,9 @@ persist.gps.qc_nlp_in_use=1 \
 persist.loc.nlp_name=com.qualcomm.location \
 ro.gps.agps_provider=1
 
+# Graphics
+debug.hwui.renderer=skiavk
+
 # iorapd
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 ro.iorapd.enable=false
