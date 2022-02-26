e34cc146bf61fb70025dadbfef3e257558b2ed43
Asif <najmulhasan3609@gmail.com>
2022-02-26 02:32:52 +0000

    rosy: Disable APEX compression

--- device.mk
+++ device.mk
@@ -16,6 +16,10 @@
 
 $(call inherit-product, vendor/xiaomi/rosy/rosy-vendor.mk)
 
+# APEX
+OVERRIDE_TARGET_FLATTEN_APEX := true
+OVERRIDE_PRODUCT_COMPRESSED_APEX := false
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
